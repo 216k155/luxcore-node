5a8d8104aba8b1a6aa7a51202aaf8918116bc4d3
sarawut <sanit.sa@outlook.com>
2018-08-30 11:16:48 -0700

    fix addressbalance emit

--- test/services/luxd.unit.js
+++ test/services/luxd.unit.js
@@ -6567,7 +6567,7 @@ describe('Lux Service', function() {
 			});
 
 
-			emitter.on('quantumd/addressbalance', function(data) {
+			emitter.on('luxd/addressbalance', function(data) {
 				data.balance.should.equal(1);
 				data.totalReceived.should.equal(2);
 				data.totalSpent.should.equal(1);
@@ -6612,7 +6612,7 @@ describe('Lux Service', function() {
 				unconfirmedBalance: 100
 			});
 
-			emitter.on('quantumd/addressbalance', function(data) {
+			emitter.on('luxd/addressbalance', function(data) {
 				data.balance.should.equal(0);
 				data.totalReceived.should.equal(10);
 				data.totalSpent.should.equal(10);
